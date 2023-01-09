fe0ae455df427c05789a8265975b0e6c39fabb0b
sergey <petrelevich@yandex.ru>
2023-01-09 21:40:55 +0300

    L02-gradle

--- L01-gradle/src/main/java/ru/otus/App.java
+++ L01-gradle/src/main/java/ru/otus/App.java
@@ -10,15 +10,13 @@ import java.util.List;
 
 
 /**
- *
  * To start the application:
  * ./gradlew build
  * java -jar ./L01-gradle/build/libs/gradleHelloWorld-0.1.jar
- *
+ * <p>
  * To unzip the jar:
  * unzip -l L01-gradle.jar
  * unzip -l gradleHelloWorld-0.1.jar
- *
  */
 public class App {
     public static void main(String... args) {
