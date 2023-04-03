b48be7a0dd62a3dd83f5f5ccaf5dd4c9d8ac204f
stvort <stvort@mail.ru>
2023-04-04 01:36:08 +0400

    L23-noSQL added

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -3,4 +3,13 @@ public interface Versions {
     String jmh = "1.36";
     String asm = "9.2";
     String glassfishJson = "2.0.1";
+
+    String lombok = "1.18.26";
+    String gson = "2.10.1";
+
+    String mongodb = "4.9.0";
+    String mongodbReactive = "4.9.0";
+    String cassandra = "4.15.0";
+    String neo4j = "5.7.0";
+    String jedis = "4.3.2";
 }
